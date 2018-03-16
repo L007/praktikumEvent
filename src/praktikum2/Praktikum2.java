fa3e230bc215d073ac0f797157dd6ea3d55a1199
L007 <antoeldi@gmail.com>
2018-03-16 14:05:26 +0700

    initial commit

---
+++ src/praktikum2/Praktikum2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum2;
+
+/**
+ *
+ * @author eldi
+ */
+public class Praktikum2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Biodata biodata = new Biodata();
+        biodata.setVisible(true);
+    }
+    
+}
